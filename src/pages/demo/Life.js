bf87d63dbeff24185d0d569ae2f9b671d28325a9
jiadongli <1347507274@qq.com>
2018-10-09 18:23:25 +0800

    eject

--- src/pages/demo/Life.js
+++ src/pages/demo/Life.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Child from './Child';
+import style from './index.less';
 export default class Life extends React.Component{
     constructor(props){
         super(props);
@@ -13,10 +14,7 @@ export default class Life extends React.Component{
         })
     }
     render(){
-        var style = {
-            padding:50
-        }
-        return <div style={style}>
+        return <div className={style.class}>
             <p>React 生命周期介绍</p>
             <button onClick={this.handleAdd}>点击一下</button>
             <p>{this.state.count}</p>
